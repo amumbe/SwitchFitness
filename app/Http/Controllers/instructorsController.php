77879f4bed4a4ec5f029c0928bcbda78509c7703
unknown <angellamumbe720@gmail.com>
2018-07-05 18:33:59 +0300

    m

--- app/Http/Controllers/instructorsController.php
+++ app/Http/Controllers/instructorsController.php
@@ -15,7 +15,7 @@ class InstructorsController extends Controller
     public function index()
     {
          //Get instroctor
-         $instructors = instructor::all();
+         $instructors = instructors::all();
 
          //return collection of instructors as a resource
          return $instructors;
